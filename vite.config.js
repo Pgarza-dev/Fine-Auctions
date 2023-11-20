4790ed05cb809cf0f0692cdfb029090f106e0266
Pablo Gaza <p.garza1983@gmail.com>
2023-11-20 19:54:38 +0100

    Update links and build configuration

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,21 @@ export default defineConfig({
       usePolling: true,
     },
   },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "src/index.html"),
+        profile: path.resolve(__dirname, "src/user/index.html"),
+        login: path.resolve(__dirname, "src/login/index.html"),
+        signup: path.resolve(__dirname, "src/signup/index.html"),
+        unauthorized: path.resolve(__dirname, "src/unauthorized/index.html"),
+        settings: path.resolve(__dirname, "src/settings/index.html"),
+        search: path.resolve(__dirname, "src/search/index.html"),
+        post: path.resolve(__dirname, "src/post/index.html"),
+        about: path.resolve(__dirname, "src/about/index.html"),
+      },
+    },
+  },
 });
