63f349a519cfcd119f900a1b842854191ddb0d0a
Pablo Gaza <p.garza1983@gmail.com>
2023-11-22 18:14:24 +0100

    Add login form functionality

--- src/page_scripts/login.js
+++ src/page_scripts/login.js
@@ -4,3 +4,37 @@ import { createFormDataObject } from "../forms/utils.js";
 import { checkLoginForm } from "../forms/validation.js";
 import { loginUser } from "../services/auth.js";
 import { clearErrors, displayErrors } from "../forms/handleErrors.js";
+
+removeActiveUser();
+
+async function handleLogin(formDataObject) {
+  const userData = {
+    email: formDataObject.email,
+    password: formDataObject.password,
+  };
+  const response = await loginUser(userData);
+
+  if (response.error) {
+    handleFormApiError([response.error]);
+  } else {
+    window.location.href = "/";
+  }
+}
+
+const loginForm = document.querySelector("#login-form");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  clearErrors();
+
+  const form = createFormDataObject(loginForm);
+
+  const loginValidationResult = checkLoginForm(form);
+
+  if (loginValidationResult.isValid) {
+    handleLogin(form);
+  } else {
+    displayErrors(loginValidationResult.errors);
+  }
+});
