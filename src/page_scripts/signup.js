64a86578e27af8c335d7d64709e162d54dbaf7b7
Pablo Gaza <p.garza1983@gmail.com>
2023-11-22 12:44:17 +0100

    Refactor code to improve performance and readability

---
+++ src/page_scripts/signup.js
@@ -0,0 +1,6 @@
+import { checkSignupForm } from "src/forms/validation.js";
+import { clearErrors, displayErrors } from "";
+import { registerUser } from "";
+import { createFormDataObject } from "";
+import { handleFormApiError } from "";
+import { removeActiveUser } from "";
