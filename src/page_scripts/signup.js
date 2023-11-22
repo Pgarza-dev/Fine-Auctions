149c84f08a7266c6d81ae37a6ac96b89a2c8e276
Pablo Gaza <p.garza1983@gmail.com>
2023-11-22 13:26:17 +0100

    Add signup functionality to login page

--- src/page_scripts/signup.js
+++ src/page_scripts/signup.js
@@ -1,6 +1,41 @@
 import { checkSignupForm } from "src/forms/validation.js";
-import { clearErrors, displayErrors } from "";
-import { registerUser } from "";
-import { createFormDataObject } from "";
-import { handleFormApiError } from "";
-import { removeActiveUser } from "";
+import { clearErrors, displayErrors } from "src/forms/handleErrors.js";
+import { registerUser } from "src/services/auth.js";
+import { createFormDataObject } from "src/forms/utils.js";
+import { handleFormApiError } from "src/forms/handleErrors.js";
+import { removeActiveUser } from "src/utils/handleLocalStorageUser.js";
+
+removeActiveUser();
+
+async function handleSignUp(formDataObject) {
+  const userData = {
+    name: formDataObject.username,
+    email: formDataObject.email,
+    password: formDataObject.password,
+  };
+  const response = await registerUser(userData);
+
+  if (response.errors) {
+    handleFormApiError(response.errors);
+  } else {
+    window.location.href = "/login/";
+  }
+}
+
+const signupForm = document.querySelector("#signup-form");
+
+signupForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  clearErrors();
+
+  const form = createFormDataObject(signupForm);
+
+  const validationResult = checkSignupForm(form);
+
+  if (validationResult.isValid) {
+    handleSignUp(form);
+  } else {
+    displayErrors(validationResult.errors);
+  }
+});
