01332c8aa1b778ed0857b5098a8ec7a41c5c7220
Pablo Gaza <p.garza1983@gmail.com>
2023-11-23 09:18:11 +0100

    Commented out redirect to index.html in handleAuth function

--- src/auth/handleAuth.js
+++ src/auth/handleAuth.js
@@ -17,6 +17,6 @@ export function handleAuth() {
 
   if (!activeUser || !accessToken) {
     console.warn("No active user or access token found, redirecting.");
-    window.location.href = "/index.html";
+    // window.location.href = "/index.html";
   }
 }
