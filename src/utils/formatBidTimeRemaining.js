f68578c715165b5c1cd6c86808f8820d9430465d
Pablo Gaza <p.garza1983@gmail.com>
2023-11-24 21:11:47 +0100

    Add handling for negative timeRemaining in formatBidTimeRemaining.js

--- src/utils/formatBidTimeRemaining.js
+++ src/utils/formatBidTimeRemaining.js
@@ -5,6 +5,9 @@ export function formatTimeRemaining(timeRemaining) {
     (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
   );
   const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  if (timeRemaining < 0) {
+    return "Auction ended";
+  }
   if (days === 0 && hours === 0 && minutes === 0) {
     return `${seconds}s`;
   }
@@ -13,6 +16,9 @@ export function formatTimeRemaining(timeRemaining) {
   }
   if (days === 0) {
     return `${hours}h  `;
+  }
+  if (days > 0) {
+    return `${days}d `;
   }
 
   return `${days}d ${hours}h ${minutes}m `;
