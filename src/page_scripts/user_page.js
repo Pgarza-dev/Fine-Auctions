929ccfb425d0cf33a243427d26c5c962df82b04e
Pablo Gaza <p.garza1983@gmail.com>
2023-11-28 22:23:20 +0100

    Refactor displayBidHistory function to improve UI

--- src/page_scripts/user_page.js
+++ src/page_scripts/user_page.js
@@ -192,15 +192,40 @@ async function displayBidHistory() {
         formatTimeRemaining(timeRemaining, (formattedTime) => {
           timeRemainingElement.textContent = formattedTime;
         });
-        return `<div class="w-full"
-      >
-        <img src="${
-          bid.listing.media[0]
-        }" alt="listing image" class="w-full"  />
-        <p >${bid.listing.title}</p>
-        <p >$${bid.amount}</p>
-        <p class="text-orange-500">${formatTimeRemaining(timeRemaining)}</p>
-      </div>`;
+        return `
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-96 md:rounded-none md:rounded-s-lg" src="${
+              bid.listing.media[0]
+            }" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${
+                  bid.listing.title
+                }</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">$${
+                  bid.amount
+                }</p>
+                <p class="mb-3 font-normal text-primary-text dark:text-gray-400">${formatTimeRemaining(
+                  timeRemaining,
+                )}</p>
+            </div>
+        </div>
+        `;
+        //   return `<div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+
+        // >
+        //   <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"" src="${
+        //     bid.listing.media[0]
+        //   }" alt="listing image"/>
+        //   <div class="flex flex-col justify-between p-4 leading-normal ">
+        //   <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" >${
+        //     bid.listing.title
+        //   }</h5>
+        //   <p >$${bid.amount}</p>
+        //   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${formatTimeRemaining(
+        //     timeRemaining,
+        //   )}</p>
+        //   </div>
+        // </div>`;
       })
       .join("");
 
